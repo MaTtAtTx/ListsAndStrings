297a1d62e6b4f92656fb66080c5e849e8c452a54
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-03 09:41:45 -0600

    Implemented popup displays into controller class and started creating a list of donuts.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -3,19 +3,32 @@ package useful.controller;
 import java.util.List;
 import java.util.ArrayList;
 import useful.model.Donut;
+import useful.view.PopupDisplay;
 
 public class ToolController
 {
 	private List<Donut> donutList;
+	private PopupDisplay display;
 	
 	public ToolController()
 	{
 		donutList = new ArrayList<Donut>();
+		display = new PopupDisplay();
 	}
 	
 	public void start()
 	{
 		Donut temp = new Donut(false, false, null, null, 0);
-		System.out.println(donutList);
+		
+		donutList.add(temp);
+	}
+	
+	private void fillTheList()
+	{
+		Donut jellyFilled = new Donut(false, false, null, "Jelly-Filled", 0);
+		Donut glazed = new Donut(false, false, null, "Glazed", 0);
+		Donut maple = new Donut(false, false, null, "Maple", 0);
+		Donut chocolate = new Donut(false, false, null, "Chocolate", 0);
+		Donut vanillaCustard = new Donut(false, false, null, "Vanilla Custard", 0);
 	}
 }
--- src/useful/view/PopupDisplay.java
+++ src/useful/view/PopupDisplay.java
@@ -2,18 +2,32 @@ package useful.view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * The popup class for CTEC :)
+ * @author Matthew Johnsen
+ * @version 1.0 03.10.2017
+ */
 public class PopupDisplay
 {
+	/**
+	 * Displays the supplied message in a popup window.
+	 * @param message The message that is displayed.
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
 	
-	public String getResponse(String questionAsked)
+	/**
+	 * Displays the supplied question in a popup and returns the answer as a String.
+	 * @param fromQuestion The question to ask the user
+	 * @return The String answer
+	 */
+	public String getResponse(String fromQuestion)
 	{
 		String response = "";
 
-		response += JOptionPane.showInputDialog(null, questionAsked);
+		response += JOptionPane.showInputDialog(null, fromQuestion);
 
 		return response;
 	}
